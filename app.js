0b8c077f25a0749139896d0c5dc8fab1e32deacd
Joy-Chang-2021 <joy50422@gmail.com>
2021-10-22 22:03:56 +0800

    (1) add & bring in movies.json (2) use handlebars loop to render page

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const app = express()
 const port = 3000
 // handlebars
 const exphbs = require('express-handlebars')
+// Movie List
+const movieList = require('./movies.json')
 
 // express template engine
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
@@ -14,7 +16,7 @@ app.use(express.static('public'))
 
 //routes setting
 app.get('/', (req, res) => {
-  res.render('index')
+  res.render('index', { movies: movieList.results })
 })
 
 app.listen(port, () => {
